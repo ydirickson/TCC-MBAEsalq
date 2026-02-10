20c17fbaea90a883d06fbd669208b973674efad3
Yuri Dirickson <yuri.dirickson@usp.br>
2026-02-09 21:59:11 -0300

    Acertos nos cenários base

---
+++ bd/c1/30_seed_turmas.sql
@@ -0,0 +1,10 @@
+INSERT INTO turma_graduacao (id, curso_id, ano, semestre, status)
+SELECT
+  to_char(ano, 'FM0000') || lpad(semestre::text, 2, '0') || c.codigo,
+  c.id,
+  ano,
+  semestre,
+  'ATIVA'
+FROM curso_graduacao c
+CROSS JOIN generate_series(2024, 2026) AS ano
+CROSS JOIN (SELECT 1 AS semestre) s;
