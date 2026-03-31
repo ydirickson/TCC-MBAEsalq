38b1d7f8b3eeae09b13594d9f3a4e1a8aa33c838
Yuri Dirickson <yuri.dirickson@usp.br>
2026-03-31 17:19:50 -0300

    Adicionando configurações para o NoOp do kafka

---
+++ servicos/diplomas/src/main/java/br/com/tcc/diplomas/kafka/KafkaNoOpConfig.java
@@ -0,0 +1,26 @@
+package br.com.tcc.diplomas.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.Map;
+
+@Configuration
+@Profile("no-kafka")
+public class KafkaNoOpConfig {
+
+  @Bean
+  KafkaTemplate<String, Object> kafkaTemplate() {
+    Map<String, Object> props = Map.of(
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+    );
+    return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(props));
+  }
+}
